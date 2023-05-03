2b369e2b6f0c27ef1ced32c74f63924c9595d9e7
YuyaMatsumoto <y.matsu.moto95@gmail.com>
2023-05-04 02:04:30 +0900

    WebSiteのタグに変換 テスト用にまずはお試しのタグ

---
+++ WebSiteTag/WebSiteTagService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneAI.WebSiteTag
+{
+    internal class WebSiteTagService
+    {
+
+        public static string ConvertChatGptContentToWebSiteTag(string chatGptContent)
+        {
+            return $@"
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    更新 : {DateTime.Today.ToString("yyyy/MM/dd HH:ss")}
+    {chatGptContent}
+  </body>
+</html>
+";
+        }
+    }
+}
