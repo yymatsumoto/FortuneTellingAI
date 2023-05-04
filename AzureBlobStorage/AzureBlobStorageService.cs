97d86d553332093e283ff9cd0204a363ba7bf71c
YuyaMatsumoto <y.matsu.moto95@gmail.com>
2023-05-05 01:40:33 +0900

    1日に複数回更新されないように

--- AzureBlobStorage/AzureBlobStorageService.cs
+++ AzureBlobStorage/AzureBlobStorageService.cs
@@ -16,19 +16,36 @@ namespace FortuneAI.AzureBlobStorage
 {
     internal class AzureBlobStorageService
     {
+        private static string _accountName = System.Environment.GetEnvironmentVariable("AccountName", EnvironmentVariableTarget.Process);
+        private static string _accessKey = System.Environment.GetEnvironmentVariable("AzureBlobStorageAccessKey", EnvironmentVariableTarget.Process);
+        private static string _containerName = System.Environment.GetEnvironmentVariable("ContainerName", EnvironmentVariableTarget.Process);
+        private static string _blobName = System.Environment.GetEnvironmentVariable("BlobName", EnvironmentVariableTarget.Process);
+
+        private CloudBlockBlob CreateCloudBlockBlobInstance()
+        {
+            var credential = new StorageCredentials(_accountName, _accessKey);
+            var storageAccount = new CloudStorageAccount(credential, true);
+            var blobServiceClient = storageAccount.CreateCloudBlobClient();
+            var container = blobServiceClient.GetContainerReference(_containerName);
+            return container.GetBlockBlobReference(_blobName);
+        }
+
+        /// <summary>
+        /// 1日1回占いが更新されるが、
+        /// 更新失敗や占いが出力されなかった場合には一日に複数回実行される
+        /// </summary>
+        public async Task<bool> CheckRequireUpdate()
+        {
+            var blockBlob = CreateCloudBlockBlobInstance();
+
+            var text = await blockBlob.DownloadTextAsync();
+            //厳密な判定ではないが、更新日時を使用してその日に更新済みかを判定する
+            return !text.Contains(DateTime.Today.ToString("yyyy/MM/dd"));
+        }
+
         public async void ReplaceFile(string content)
         {
-            var accountName = System.Environment.GetEnvironmentVariable("AccountName", EnvironmentVariableTarget.Process); ;
-            var accessKey = System.Environment.GetEnvironmentVariable("AzureBlobStorageAccessKey", EnvironmentVariableTarget.Process);
-            var containerName = System.Environment.GetEnvironmentVariable("ContainerName", EnvironmentVariableTarget.Process);
-            var blobName = System.Environment.GetEnvironmentVariable("BlobName", EnvironmentVariableTarget.Process);
-
-            var credential = new StorageCredentials(accountName, accessKey);
-            var storageAccount = new CloudStorageAccount(credential, true);
-
-            var blobServiceClient = storageAccount.CreateCloudBlobClient();
-            var container = blobServiceClient.GetContainerReference(containerName);
-            var blockBlob = container.GetBlockBlobReference(blobName);
+            var blockBlob = CreateCloudBlockBlobInstance();
 
             blockBlob.Properties.ContentType = "text/html";
             await blockBlob.UploadTextAsync(content);
--- FortuneTelling.cs
+++ FortuneTelling.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using FortuneAI.AzureBlobStorage;
 using FortuneAI.WebSiteTag;
 using Microsoft.Azure.WebJobs;
@@ -12,7 +13,12 @@ namespace FortuneAI.Function
         [FunctionName("FortuneTelling")]
         public static void Run([TimerTrigger("0 0 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
+            var requireUpdate = new AzureBlobStorageService().CheckRequireUpdate();
+            if (!requireUpdate.Result) { return; }
+
             var chatGptContent = new FortuneAI.ChatGpt.ChatGptApiService().GetFortuneTelling();
+            //厳密な判定ではないが、12行以上なければ生成失敗として更新しない
+            if (chatGptContent.Result.Split("\n\n").Count() < 12) { return; }
             new AzureBlobStorageService().ReplaceFile(WebSiteTagService.ConvertChatGptContentToWebSiteTag(chatGptContent.Result));
         }
     }
